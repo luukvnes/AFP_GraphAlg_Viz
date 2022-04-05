158216222e5b35e707605c37ef242c61eca3b8ad
luukvnes <34107188+luukvnes@users.noreply.github.com>
2022-04-05 15:34:42 +0200

    update

--- GraphAlgViz/src/Gif.hs
+++ GraphAlgViz/src/Gif.hs
@@ -4,13 +4,14 @@ import Codec.Picture.Gif
 import Codec.Picture
 import System.Directory
 import Helper
+import Data.List
 
 attemptToCreateGif :: [Char] -> IO ()
 attemptToCreateGif gifPath = do
     imageFolderDirs <- listDirectory "resultFolder/ImageFolders"
     let imageFolderDir = "resultFolder/ImageFolders/"  ++ head imageFolderDirs ++ "/"
     files <- listDirectory imageFolderDir
-    let fullPaths  = map (imageFolderDir ++) files -- Creates all the paths to the images to be converted into a gif
+    let fullPaths  = map (imageFolderDir ++) (reverse $ sort files) -- Creates all the paths to the images to be converted into a gif
     eImgs <- mapM readBitmap (reverse fullPaths) -- converts them to (Either str DynamicImage)
     let palDelayImgs = map ((\(x,y) -> (y, 100, x)) . palettize paletteOptions . convertRGB8 . extractImage) eImgs
     let gifWithError = writeGifImages gifPath LoopingNever palDelayImgs -- writes the gif to a file and returns something
